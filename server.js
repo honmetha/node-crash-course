13d9b7e5450adc0ac64a82a8a6aa48d4442692a4
Hon <dennis.mejen@gmail.com>
2021-08-09 01:19:07 +0700

    [Added] 4 - Requests & Responses: response object

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method);
+
+  // set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  res.write('<head><link rel="stylesheet" href="#"></head>');
+  res.write("<p>hello, ninjas</p>");
+  res.write("<p>hello again, ninjas</p>");
+  res.end();
 });
 
 server.listen(3000, "localhost", () => {
