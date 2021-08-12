fff1c34c375613837fb6f55e3f28e9b78f7064d6
Hon <dennis.mejen@gmail.com>
2021-08-13 00:35:05 +0700

    [Added] 6 - Express Apps: redirects & 404 pages

--- app.js
+++ app.js
@@ -7,11 +7,19 @@ const app = express();
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  // res.send("<p>home page</p>");
   res.sendFile("./views/index.html", { root: __dirname });
 });
 
 app.get("/about", (req, res) => {
-  // res.send("<p>about page</p>");
   res.sendFile("./views/about.html", { root: __dirname });
+});
+
+// redirects
+app.get("/about-us", (req, res) => {
+  res.redirect("/about");
+});
+
+// 404 page
+app.use((req, res) => {
+  res.status(404).sendFile("./views/404.html", { root: __dirname });
 });
