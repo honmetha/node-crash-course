21ef73f6c0ad830f7b69eeb20b49f1892873ddbc
Hon <dennis.mejen@gmail.com>
2021-08-18 23:26:21 +0700

    [Added] 8 - Middleware: what is middleware?

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.set("view engine", "ejs");
 // listen for requests
 app.listen(3000);
 
+app.use((req, res) => {
+  console.log("new request made:");
+  console.log(`host: `, req.hostname);
+  console.log(`path: `, req.path);
+  console.log(`method: `, req.method);
+});
+
 app.get("/", (req, res) => {
   const blogs = [
     {
