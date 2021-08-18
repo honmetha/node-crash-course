6a81e83c874743d2f71a9ee3eb27369ed7bed25b
Hon <dennis.mejen@gmail.com>
2021-08-19 00:10:18 +0700

    [Added] 8 - Middleware: 3rd party middleware

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const morgan = require("morgan");
 
 // express app
 const app = express();
@@ -9,13 +10,7 @@ app.set("view engine", "ejs");
 // listen for requests
 app.listen(3000);
 
-app.use((req, res, next) => {
-  console.log("new request made:");
-  console.log(`host: `, req.hostname);
-  console.log(`path: `, req.path);
-  console.log(`method: `, req.method);
-  next();
-});
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -33,11 +28,6 @@ app.get("/", (req, res) => {
     },
   ];
   res.render("index", { title: "Home", blogs });
-});
-
-app.use((req, res, next) => {
-  console.log("in the next middleware");
-  next();
 });
 
 app.get("/about", (req, res) => {
