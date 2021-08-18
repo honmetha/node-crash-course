c8e637ec9415084b365e349c225c62c4f3b3e1b2
Hon <dennis.mejen@gmail.com>
2021-08-18 23:33:08 +0700

    [Added] 8 - Middleware: using next()

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ app.set("view engine", "ejs");
 // listen for requests
 app.listen(3000);
 
-app.use((req, res) => {
+app.use((req, res, next) => {
   console.log("new request made:");
   console.log(`host: `, req.hostname);
   console.log(`path: `, req.path);
   console.log(`method: `, req.method);
+  next();
 });
 
 app.get("/", (req, res) => {
@@ -32,6 +33,11 @@ app.get("/", (req, res) => {
     },
   ];
   res.render("index", { title: "Home", blogs });
+});
+
+app.use((req, res, next) => {
+  console.log("in the next middleware");
+  next();
 });
 
 app.get("/about", (req, res) => {
