47edcc95c7aa062f12199073a3f57979bdee1d14
Hon <dennis.mejen@gmail.com>
2021-08-21 23:49:46 +0700

    [Added] 9 - MongoDB: getting & saving data

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 // express app
 const app = express();
 
 // connect to mongodb
 const dbURI =
-  "mongodb+srv://honmetha:A1b2C3d4@nodetuts.qvsht.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+  "mongodb+srv://honmetha:A1b2C3d4@nodetuts.qvsht.mongodb.net/node-tuts?retryWrites=true&w=majority";
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => app.listen(3000))
@@ -19,6 +20,44 @@ app.set("view engine", "ejs");
 // middleware & static files
 app.use(express.static("public"));
 app.use(morgan("dev"));
+
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog2",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(`err`, err);
+    });
+});
+
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(`err`, err);
+    });
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("61212c5f05728f404790a896")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(`err`, err);
+    });
+});
 
 app.get("/", (req, res) => {
   const blogs = [
