34be124fa3cc4f223ef6b08067ba62af6879661c
Hon <dennis.mejen@gmail.com>
2021-08-22 16:47:19 +0700

    [Added] 10 - Get, Post & Delete Requests: request types and post requests

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.set("view engine", "ejs");
 
 // middleware & static files
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 // routes
@@ -40,6 +41,17 @@ app.get("/blogs", (req, res) => {
     .catch((err) => {
       console.log(`err`, err);
     });
+});
+
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => console.log(`err`, err));
 });
 
 app.get("/blogs/create", (req, res) => {
